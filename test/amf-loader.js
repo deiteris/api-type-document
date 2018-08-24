648a4c879b1b6be413c0ce05ecee61cb1a491804
Pawel Psztyc <jarrodek@gmail.com>
2018-08-24 14:45:58 -0700

    Update: Adding support for compact model

--- test/amf-loader.js
+++ test/amf-loader.js
@@ -3,7 +3,7 @@ AmfLoader.load = function(typeIndex) {
   typeIndex = typeIndex || 0;
   const url = location.protocol + '//' + location.host +
     location.pathname.substr(0, location.pathname.lastIndexOf('/'))
-    .replace('/test', '/demo') + '/amf-model.json';
+    .replace('/test', '/demo') + '/demo-api.json';
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', (e) => {
