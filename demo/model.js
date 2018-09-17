43509e8bfeb95e4beaa19262619daf4381757745
Pawel Psztyc <jarrodek@gmail.com>
2018-09-17 13:15:28 -0700

    Fix: Fixing APIC-83

--- demo/model.js
+++ demo/model.js
@@ -2,6 +2,7 @@ const generator = require('@api-components/api-model-generator');
 
 const files = new Map();
 files.set('demo-api/demo-api.raml', 'RAML 1.0');
+files.set('apic-83/apic-83.raml', 'RAML 1.0');
 
 generator(files)
 .then(() => console.log('Finito'));
