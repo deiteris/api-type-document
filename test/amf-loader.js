ae5f6c62b4b55c94f59d34ca12d4012118ccc029
Pawel Psztyc <jarrodek@gmail.com>
2018-11-27 12:16:35 -0800

    Fix: Fixing nil and any shape recognition

--- test/amf-loader.js
+++ test/amf-loader.js
@@ -1,9 +1,9 @@
 const AmfLoader = {};
-AmfLoader.load = function(typeIndex) {
-  typeIndex = typeIndex || 0;
+AmfLoader.load = function(compact) {
+  const file = '/demo-api' + (compact ? '-compact' : '') + '.json';
   const url = location.protocol + '//' + location.host +
     location.pathname.substr(0, location.pathname.lastIndexOf('/'))
-    .replace('/test', '/demo') + '/demo-api.json';
+    .replace('/test', '/demo') + file;
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', (e) => {
@@ -14,12 +14,42 @@ AmfLoader.load = function(typeIndex) {
         reject(e);
         return;
       }
-      const def = data[0]['http://a.ml/vocabularies/document#declares'][typeIndex];
-      resolve([data, def]);
+      resolve(data);
     });
     xhr.addEventListener('error',
       () => reject(new Error('Unable to load model file')));
     xhr.open('GET', url);
     xhr.send();
+  });
+};
+
+AmfLoader.loadType = function(name, compact) {
+  return AmfLoader.load(compact)
+  .then((amf) => {
+    const data = amf;
+    if (amf instanceof Array) {
+      amf = amf[0];
+    }
+    const ns = ApiElements.Amf.ns;
+    const key = compact ? 'doc:declares' : ns.raml.vocabularies.document + 'declares';
+    const defs = amf[key];
+    for (let i = 0; i < defs.length; i++) {
+      let type = defs[i];
+      if (type instanceof Array) {
+        type = type[0];
+      }
+      const key = compact ? 'shacl:name' : ns.w3.shacl.name + 'name';
+      let nameData = type[key];
+      if (!nameData) {
+        continue;
+      }
+      if (nameData instanceof Array) {
+        nameData = nameData[0];
+      }
+      const typeName = nameData['@value'];
+      if (typeName === name) {
+        return [data, type];
+      }
+    }
   });
 };
