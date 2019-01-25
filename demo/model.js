0ce5d8dc59015be58d617a3a4ca8bdd47a94611c
Pawel Psztyc <jarrodek@gmail.com>
2019-01-24 17:13:12 -0800

    Update: Updating model generator and AMF models

--- demo/model.js
+++ demo/model.js
@@ -1,9 +1,4 @@
 const generator = require('@api-components/api-model-generator');
-
-const files = new Map();
-files.set('demo-api/demo-api.raml', 'RAML 1.0');
-files.set('apic-83/apic-83.raml', 'RAML 1.0');
-files.set('oas-api/Petstore.yaml', 'OAS 2.0');
-
-generator(files)
-.then(() => console.log('Finito'));
+generator('./demo/apis.json')
+.then(() => console.log('Models created'))
+.catch((cause) => console.error(cause));
